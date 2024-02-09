c1bfce1b767a856f8051c37f834607c01638a46a
benkinard <bkinard@fastrics.com>
2024-02-09 09:06:11 -0500

    new config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_J6ZzbuSrk',
+        userPoolClientId: '6foqabhsccki6bkd6jvtoiseou',
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: ''
+    }
+};
