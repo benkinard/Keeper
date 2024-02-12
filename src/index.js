379f012d32d737e0e9ecaae423be2424403d4750
benkinard <bkinard@fastrics.com>
2024-02-12 08:54:58 -0500

    Add Home index route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./components/App";
+import App from "./routes/App";
+import Home from "./routes/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
     }
 ]);
 
